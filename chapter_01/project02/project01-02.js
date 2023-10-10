9ff6d668c3a6b4493532ff08a08b89eb7caf5ce2
Sarah Cohen <cohen.sarah591@gmail.com>
2023-10-10 19:50:11 -0400

    defined variables

--- chapter_01/project02/project01-02.js
+++ chapter_01/project02/project01-02.js
@@ -2,10 +2,18 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Sarah Cohen
+    Date: 10/10/2023   
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+service1Name = "Basic";
+service2Name = "Express";
+service3Name = "Extreme";
+service4Name = "Ultimate";
+service1Speed = "0 Mbps"; 
+service2Speed = "100 Mbps"; 
+service3Speed = "500 Mbps"; 
+service4Speed = "1 Gig";
